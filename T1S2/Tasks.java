603005a3f12703d5a6720dce8d92f01793df1444
mgrom-dev <kkdvkg@ya.ru>
2023-11-30 23:53:37 +0700

    complete all tasks from T1S2

--- T1S2/Tasks.java
+++ T1S2/Tasks.java
@@ -12,25 +12,131 @@ public class Tasks {
             this.max = max;
         }
     }
+    /** класс с методами сортировки */
+    public static class Sort {
+        /**
+         * Перенос самого большого значения элемента в массиве в правый край методом выбора
+         * @param array - массив целых чисел
+         * @return - новый массив 
+         */
+        public static Integer[] moveSelect(Integer[] array) {
+            int indexMaxValue = 0;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] > array[indexMaxValue]) {
+                    indexMaxValue = i;
+                }
+            }
+            Integer temp = array[indexMaxValue];
+            array[indexMaxValue] = array[array.length - 1];
+            array[array.length - 1] = temp;
+            return array;
+        }
+        
+        /**
+         * Перенос самого большого значения элемента в массиве в правый край методом пузырька
+         * @param array - массив целых чисел
+         * @return - новый массив 
+         */
+        public static Integer[] moveBubble(Integer[] array) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    Integer temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+            return array;
+        }
+
+        /**
+         * Сортировка массива методом выбора
+         * @param array - массив целых чисел
+         * @return - новый массив 
+         */
+        public static Integer[] move(Integer[] array) {
+            for (int end = array.length; end > 0; end--) {
+                int indexMaxValue = 0;
+                for (int i = 1; i < end; i++) {
+                    if (array[i] > array[indexMaxValue]) {
+                        indexMaxValue = i;
+                    }
+                }
+                Integer temp = array[indexMaxValue];
+                array[indexMaxValue] = array[end - 1];
+                array[end - 1] = temp;
+            }
+            return array;
+        }
+
+        /**
+         * Сортировка массива методом пузырька от меньшего к большему
+         * @param array - массив целых чисел
+         * @return - новый массив 
+         */
+        public static Integer[] bubble(Integer[] array) {
+            boolean repeat = true;
+            while (repeat) {
+                repeat = false;
+                for (int i = 0; i < array.length - 1; i++) {
+                    if (array[i] > array[i + 1]) {
+                        Integer temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        repeat = true;
+                    }
+                }
+            }
+            return array;
+        }
+    }
 
     public static void main(String[] args) {
         Integer[] vector1 = {1, 2, 3, 4};
         Integer[] vector2 = {5, 6, 7, 8};
-        System.out.println(String.format("Задача 1А. Скалярное произведение вектора %s и вектора %s, равно: %d", 
+        System.out.println(String.format("\nЗадача 1А. Скалярное произведение вектора %s и вектора %s, равно: %d", 
             arrayToString(vector1), arrayToString(vector2), scalarProduct(vector1, vector2)));
         
+        Integer[] randomArray = Arrays.stream( new Random().ints(10, -100, 100).toArray() ).boxed().toArray( Integer[]::new );
+        System.out.println(String.format("\nЗадача 1В. В массиве %s размером %d элементов, количество положительных чисел равно %d", 
+           arrayToString(randomArray), randomArray.length, countPositiveNumber(randomArray)));
+
+        randomArray = Arrays.stream( new Random().ints(10, 0, 100).toArray() ).boxed().toArray( Integer[]::new );
+        System.out.println(String.format("\nЗадача 1C. В массиве %s размером %d элементов, сумма всех элементов с нечетными индексами равна %d", 
+           arrayToString(randomArray), randomArray.length, summOddIndexesFromArray(randomArray)));
+
+        System.out.println(String.format("\nЗадача 1D. В массиве %s размером %d элементов, сумма всех элементов с нечетными значениями равна %d", 
+           arrayToString(randomArray), randomArray.length, summOddValuesFromArray(randomArray)));
+
+        System.out.println(String.format("\nЗадача 2A. Дан массив %s размером %d элементов. После переноса максимального числа в конец массива методом выбора, массив стал %s", 
+           arrayToString(randomArray), randomArray.length, arrayToString(Sort.moveSelect(randomArray))));
+        randomArray = Arrays.stream( new Random().ints(10, 1, 100).toArray() ).boxed().toArray( Integer[]::new );
+        System.out.println(String.format("Задача 2A. Дан массив %s размером %d элементов. После переноса максимального числа в конец массива методом пузырька, массив стал %s", 
+           arrayToString(randomArray), randomArray.length, arrayToString(Sort.moveBubble(randomArray))));
+
+        randomArray = Arrays.stream( new Random().ints(10, 1, 100).toArray() ).boxed().toArray( Integer[]::new );
+        System.out.println(String.format("\nЗадача 2В. Дан массив %s размером %d элементов. После сортировки массива методом выбора, массив стал %s", 
+           arrayToString(randomArray), randomArray.length, arrayToString(Sort.move(randomArray))));
+        randomArray = Arrays.stream( new Random().ints(10, 1, 100).toArray() ).boxed().toArray( Integer[]::new );
+        System.out.println(String.format("Задача 2В. Дан массив %s размером %d элементов. После сортировки массива методом пузырька, массив стал %s", 
+           arrayToString(randomArray), randomArray.length, arrayToString(Sort.bubble(randomArray))));
+        
+        randomArray = Arrays.stream( new Random().ints(10, 1, 100).toArray() ).boxed().toArray( Integer[]::new );
+        MinMaxIndex minMaxValues = minMaxValuesFromArray(randomArray);
+        System.out.println(String.format("\nЗадача 2C. Дан массив %s размером %d элементов. Минимальное значение в массиве %d, максимальное значение в массиве %d", 
+           arrayToString(randomArray), randomArray.length, minMaxValues.min, minMaxValues.max));
+
         Integer[] minMaxArray = Arrays.stream( new Random().ints(10, 0, 100).toArray() ).boxed().toArray( Integer[]::new );
         MinMaxIndex minMaxIndex = findMinAndMaxValue(minMaxArray);
-        System.out.println(String.format("Домашнее задание 1. Задан массив %s размером %d элементов\nИндекс минимального значения в массиве %d\nИндекс максимального значения в массиве %d", 
+        System.out.println(String.format("\nДомашнее задание 1. Задан массив %s размером %d элементов\nИндекс минимального значения в массиве %d\nИндекс максимального значения в массиве %d", 
            arrayToString(minMaxArray), minMaxArray.length, minMaxIndex.min, minMaxIndex.max));
 
-        System.out.println(String.format("Домашнее задание 2. Задан массив %s размером %d элементов\nМассив в обратном порядке %s", 
+        System.out.println(String.format("\nДомашнее задание 2. Задан массив %s размером %d элементов\nМассив в обратном порядке %s", 
            arrayToString(minMaxArray), minMaxArray.length, arrayToString(revert(minMaxArray))));
 
-        System.out.println(String.format("Домашнее задание 3. Задан массив %s размером %d элементов\nСумма элементов массива, лежащих между максимальным и минимальным по значению элементами, равна: %d", 
+        System.out.println(String.format("\nДомашнее задание 3. Задан массив %s размером %d элементов\nСумма элементов массива, лежащих между максимальным и минимальным по значению элементами, равна: %d", 
            arrayToString(minMaxArray), minMaxArray.length, summAboveMinAndMaxArray(minMaxArray)));
 
-        System.out.println(String.format("Домашнее задание 4. Задан массив %s размером %d элементов\nCреднее арифметическое среди всех элементов массива: %.1f", 
+        System.out.println(String.format("\nДомашнее задание 4. Задан массив %s размером %d элементов\nCреднее арифметическое среди всех элементов массива: %.1f\n", 
            arrayToString(minMaxArray), minMaxArray.length, average(minMaxArray)));
     }
 
@@ -63,6 +169,67 @@ public class Tasks {
             scalarProduct += vector1[i] * vector2[i];
         }
         return scalarProduct;
+    }
+
+    /**
+     * Подсчет количества положительных чисел в массиве
+     * @param array - массив целых чисел
+     * @return - количество положительных чисел
+     */
+    public static int countPositiveNumber(Integer[] array) {
+        int count = 0;
+        for (Integer number : array) {
+            if (number >= 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    /**
+     * Сумма всех элементов массива с нечетными индексами
+     * @param array - массив целых чисел
+     * @return - сумма
+     */
+    public static int summOddIndexesFromArray(Integer[] array) {
+        int summ = 0;
+        for (int i = 1; i < array.length; i += 2) {
+            summ += array[i];
+        }
+        return summ;
+    }
+
+    /**
+     * Сумма всех элементов массива с нечетными значениями
+     * @param array - массив целых чисел
+     * @return - сумма
+     */
+    public static int summOddValuesFromArray(Integer[] array) {
+        int summ = 0;
+        for (int i = 0; i < array.length; i ++) {
+            if (array[i] % 2 == 1) {
+                summ += array[i];
+            }
+        }
+        return summ;
+    }
+
+    /**
+     * Нахождение максимального и минимального элемента массива
+     * @param array - массив целых чисел
+     * @return - возвращает объект со свойствами min и max
+     */
+    public static MinMaxIndex minMaxValuesFromArray(Integer[] array) {
+        MinMaxIndex result = new MinMaxIndex(array[0], array[0]);
+        for (int i = 0; i < array.length; i ++) {
+            if (array[i] > result.max) {
+                result.max = array[i];
+            }
+            if (array[i] < result.min) {
+                result.min = array[i];
+            }
+        }
+        return result;
     }
 
     /**
