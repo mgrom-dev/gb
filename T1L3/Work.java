dd0d729bc1ef5fb127525949407322e8271450e4
mgrom-dev <kkdvkg@ya.ru>
2023-12-01 14:54:01 +0700

    rebuild work T1L3 with function

--- T1L3/Work.java
+++ T1L3/Work.java
@@ -1,24 +1,13 @@
 public class Work {
     public static void main(String[] args) {
         int[] array = new int[10];
-        int size = array.length;
-        int nLargest = 2;
-        int maxNumber = 0;
+        int maxNumberInOrder = 2;
         String message = "В массиве: ";
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 100);
         }
-        message += intArrayToString(array) + ", " + nLargest + " максимальное по счету число равняется ";
-        for (int repeat = 0; repeat < nLargest; repeat++) {
-            int maxNumberIndex = 0;
-            for (int i = 0; i < size; i++) {
-                if (array[i] > array[maxNumberIndex]) {
-                    maxNumberIndex = i;
-                }
-            }
-            maxNumber = array[maxNumberIndex];
-            array[maxNumberIndex] = 0;
-        }
+        message += intArrayToString(array) + ", " + maxNumberInOrder + " максимальное по счету число равняется ";
+        int maxNumber = findMaxValueInIntArray(array, maxNumberInOrder);
         System.out.println(message + maxNumber);
     }
 
@@ -31,5 +20,25 @@ public class Work {
             result += array[i];
         }
         return result + "]";
+    }
+
+    public static int findMaxValueInIntArray(int[] array, int maxNumberInOrder) {
+        int maxNumber = 0;
+        int size = array.length;
+        for (int repeat = 0; repeat < maxNumberInOrder; repeat++) {
+            int maxNumberIndex = 0;
+            for (int i = 0; i < size; i++) {
+                if (array[i] > array[maxNumberIndex]) {
+                    maxNumberIndex = i;
+                }
+            }
+            maxNumber = array[maxNumberIndex];
+            array[maxNumberIndex] = 0;
+        }
+        return maxNumber;
+    }
+
+    public static int findMaxValueInIntArray(int[] array) {
+        return findMaxValueInIntArray(array, 1);
     }
 }
