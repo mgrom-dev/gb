2c6d9704cd3e4fd399ba2783a26405aa0b62bd0e
mgrom-dev <kkdvkg@ya.ru>
2023-12-21 10:14:59 +0700

    add task1 and task2 for seminar

---
+++ T3/T3S1/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Определяем является ли в первое число квадратом второго. Введите два числа");
+try
+{
+    int number1 = ReadInt();
+    int number2 = ReadInt();
+    if (number2 * number2 == number1)
+    {
+        Console.WriteLine("Первое число является квадратом второго");
+    }
+    else
+    {
+        Console.WriteLine("Первое число не является квадратом второго");
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Ошибка: введенное значение не является целым числом");
+}
+Console.Write("Press any key to continue...");
+Console.ReadKey(true);
+Console.Clear();
+
+// Чтение целого значения
+int ReadInt()
+{
+    Console.Write("Введите целое число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
---
+++ T3/T3S1/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите целое число N");
+try
+{
+    int number = ReadInt();
+    int i = number * -1;
+    Console.Write(number + " => " + i);
+    while(i < number)
+    {
+        i = i + 1;
+        Console.Write(", " + i);
+    }
+    Console.WriteLine();
+}
+catch (FormatException)
+{
+    Console.WriteLine("Ошибка: введенное значение не является целым числом");
+}
+Console.Write("Press any key to continue...");
+Console.ReadKey(true);
+Console.Clear();
+
+// Чтение целого значения
+int ReadInt()
+{
+    Console.Write("Введите целое число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
