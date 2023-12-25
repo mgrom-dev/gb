d2918f76271c6981395bf46c1146a83b576c273d
mgrom-dev <kkdvkg@ya.ru>
2023-12-25 22:35:10 +0700

    add 2 seminar C#

---
+++ T3/T3S2/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int number;
+while(true)
+{
+    Console.Write("Введите трёхзначное число: ");
+    string input = Console.ReadLine()!;
+    if (int.TryParse(input, out number) && number > 99 && number < 1000)
+    {
+        break;
+    }
+    else 
+    {
+        Console.WriteLine("Не верный ввод!");
+    }
+}
+number = number / 100 * 10 + number % 10;
+Console.WriteLine(number);
