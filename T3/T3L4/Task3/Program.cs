c1775be5a0fb91b5af7298d780e3ad435094e248
mgrom-dev <kkdvkg@ya.ru>
2024-01-16 22:13:36 +0700

    add T3L4

---
+++ T3/T3L4/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// функция формирует новую строку состоящую из букв исходной строки
+static string GetLettersFromString(string str){
+    string result = "";
+    foreach(char c in str){
+        if (char.IsAsciiLetter(c))
+            result += c;
+    }
+    return result;
+}
+
+Console.Write("Введите строку: ");
+string str = Console.ReadLine()!;
+Console.Write("Строка без цифр: ");
+Console.WriteLine(GetLettersFromString(str));
