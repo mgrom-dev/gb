18c16dceab7aef14935afc9623148b3be94be5c8
mgrom-dev <kkdvkg@ya.ru>
2024-02-08 20:33:36 +0700

    remove recursive

--- T5/T5S1/home4.java
+++ T5/T5S1/home4.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 class home4 {
@@ -6,59 +8,38 @@ class home4 {
         Scanner scanner = new Scanner(System.in);
         String input = "";
         while (!input.toLowerCase().contains("exit")) {
-            System.out.println(
-                    "Введите уравнение вида: q + w = e, где q, w, e >= 0. Некоторые цифры можно заменить знаком вопроса, например: 2? + ?5 = 35. Для выхода введите exit:");
+            System.out.println("Введите уравнение вида: q + w = e, где q, w, e >= 0.");
+            System.out.println("Вместо цифр можно ставить ?, например: 2? + ?5 = 35. Для выхода введите exit:");
             input = scanner.nextLine();
-            // проверяем введеную строку на соотвествие уровнению
+            // 1. проверяем введеную строку на соотвествие уравнению
+            // 2. разбиавем строку по символам и запоминаем позиции всех символов ?
+            // 3. вычисляем количество всех комбинации в зависимости от количества вопросов
+            // 4. перебираем комбинации и проверяем решилось ли уравнение
             if (input.matches(" *[\\d?]+ *\\+ *[\\d?]+ *= *[\\d?]+")) {
-                // разбиваем строку по членам уравнения
-                String[] elements = input.replaceAll(" ", "").split("[+=]");
-                // ищем решения уравнения
-                String[] solved = calcExpression(elements);
-                if (isSolved(solved)) {
-                    // меняем символы члены выражения с ? на найденное решение
-                    for (int i = 0; i < elements.length; i++) {
-                        // \\Q и \\E - экранирование регулярного выражения, чтобы оно воспринималось как
-                        // строка
-                        input = input.replaceFirst("\\Q" + elements[i] + "\\E", solved[i]);
-                    }
-                    System.out.printf("Решение уравнения: %s\n", input);
-                } else {
-                    System.out.println("У уравнения нет решений!");
+                char[] charArray = input.toCharArray();
+                List<Integer> questionPositions = new ArrayList<>();
+                String solved = "";
+                for (int i = 0; i < charArray.length; i++)
+                    if (charArray[i] == '?')
+                        questionPositions.add(i);
+                for (int variant = 0, max = (int) Math.pow(10, questionPositions.size()); variant < max; variant++) {
+                    char[] numbers = String.format("%0" + questionPositions.size() + "d", variant).toCharArray();
+                    for (int v = 0; v < questionPositions.size(); v++)
+                        charArray[questionPositions.get(v)] = numbers[v];
+                    if (isSolved(new String(charArray)))
+                        solved = new String(charArray);
                 }
-            }
+                System.out.println(solved.isEmpty() ? "У уравнения нет решений!" : "Решение уравнения: " + solved);
+            } else if (!input.toLowerCase().contains("exit"))
+                System.out.println("Ошибка ввода выражения, попробуйте еще раз.");
         }
         scanner.close();
     }
 
     // проверяем решено ли равенство
-    public static boolean isSolved(String[] elements) {
-        try {
-            return Integer.parseInt(elements[0]) + Integer.parseInt(elements[1]) == Integer.parseInt(elements[2]);
-        } catch (Exception e) {
-            // если в элементах присутствует знак ? вызывается exception, соотвественно
-            // равенсто не равно
-            return false;
-        }
-    }
-
-    // рекурсивная функция где мы последовательно меняем знаки ? на числа от 0 до 9
-    public static String[] calcExpression(String[] elements) {
-        for (int i = 0; i < elements.length; i++) {
-            // если в текущем элементе присуствует ? то нужно попробовать его заменить
-            if (elements[i].indexOf("?") != -1) {
-                for (int j = 0; j < 10; j++) {
-                    // создаем копию наших элементов и меняем знак ? на число от 0 до 9 и вызываем
-                    // данную функцию еще раз
-                    String[] components = Arrays.copyOf(elements, elements.length);
-                    components[i] = components[i].replaceFirst("\\?", "" + j);
-                    components = calcExpression(components);
-                    if (isSolved(components))
-                        return components;
-                }
-            }
-        }
-        // если в уравнении нет больше ?, значит дальше нужно будет проверить его
-        return elements;
+    public static boolean isSolved(String equation) {
+        int[] equationTerms = Arrays.stream(equation.replaceAll(" ", "").split("[+=]"))
+                .mapToInt(Integer::parseInt).toArray();
+        return equationTerms[0] + equationTerms[1] == equationTerms[2];
     }
 }
