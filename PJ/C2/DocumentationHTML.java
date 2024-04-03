713dfd469918c83916a88dda35a428bc3906437d
mgrom-dev <kkdvkg@ya.ru>
2024-04-03 14:56:35 +0700

    add some pages

---
+++ PJ/C2/DocumentationHTML.java
@@ -0,0 +1,21 @@
+//: object/Documentation1.java
+/** Комментарий к классу */
+public class DocumentationHTML {
+    /**
+     * <pre>
+     * System.out.pnintln(new Date());
+     * </pre>
+     */
+    public int i;
+
+    /**
+     * Можно <em>даже</em> вставить список:
+     * <ol>
+     * <li>Пункт первый
+     * <li>Пункт второй
+     * <li>Пункт третий
+     * </ol>
+     */
+    public void f() {
+    }
+} /// :~
--- PJ/README.md
+++ PJ/README.md
@@ -7,3 +7,4 @@
 - [Книги и учебные материалы по Java, Kotlin, Scala, C, C++ (МindView)](https://www.mindviewllc.com/)
 - [Загрузка JAVA SDK (Javasun)](https://www.oracle.com/java/technologies/downloads/)
 - [Философия JAVA (Брюс Эккель)](https://drive.google.com/file/d/1mQzuKSg5fvzxFiRR5-Eg1zKa-tWZauaw/view?usp=sharing)
+- [Гитхаб с примерами из книжки Философия JAVA](https://github.com/BruceEckel/TIJ4-code/tree/master)
