2bf9ba783f3b6a43fbf8df688c9b32391560749d
mgrom-dev <kkdvkg@ya.ru>
2024-06-11 00:01:29 +0700

    new seminar

---
+++ T11/T11S6/Fork.java
@@ -0,0 +1,11 @@
+public class Fork {
+    private boolean using;
+
+    public boolean isUsing() {
+        return using;
+    }
+
+    public void setUsing(boolean using) {
+        this.using = using;
+    }
+}
