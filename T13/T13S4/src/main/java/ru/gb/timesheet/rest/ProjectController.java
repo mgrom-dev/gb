8da5bfd8016755dec855994170ffe34fda5ff4ab
mgrom-dev <mgrom-dev@ya.ru>
2024-07-24 14:43:20 +0700

    add new lection

--- T13/T13S4/src/main/java/ru/gb/timesheet/rest/ProjectController.java
+++ T13/T13S4/src/main/java/ru/gb/timesheet/rest/ProjectController.java
@@ -3,6 +3,10 @@ package ru.gb.timesheet.rest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import ru.gb.timesheet.model.Project;
 import ru.gb.timesheet.model.Timesheet;
 import ru.gb.timesheet.service.ProjectService;
@@ -20,11 +24,16 @@ public class ProjectController {
     this.service = service;
   }
 
+  @Operation(summary = "Получить проект по ID", description = "Возвращает проект с указанным ID. Если проект не найден, возвращает 404.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Проект найден"),
+      @ApiResponse(responseCode = "404", description = "Проект не найден")
+  })
   @GetMapping("/{id}")
   public ResponseEntity<Project> get(@PathVariable Long id) {
     return service.getById(id)
-      .map(ResponseEntity::ok)
-      .orElseGet(() -> ResponseEntity.notFound().build());
+        .map(ResponseEntity::ok)
+        .orElseGet(() -> ResponseEntity.notFound().build());
   }
 
   @GetMapping("/{id}/timesheets")
